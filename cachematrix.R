50aaf5a67a63caab8ad1400c306472eee7dfed31
grdym <andrp2000@gmail.com>
2015-01-24 17:07:28 -0800

    Re-formatted cachematrix.R
    Added some spacing to the code in cache matrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -28,8 +28,8 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
     m <- x$getsolve()
     if(!is.null(m)) {
-      message("getting cached data")
-      return(m)
+        message("getting cached data")
+        return(m)
     }
     data <- x$get()
     m <- solve(data, ...)
